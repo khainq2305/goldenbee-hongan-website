8052584dfbe1a44f7ed9fe2bce3483f3af52c282
Doan Thanh <dtt2145@gmail.com>
2025-07-08 10:38:58 +0700

    #35 - thanhdt2145 - page:new_address

--- src/app/(account)/layout.tsx
+++ src/app/(account)/layout.tsx
@@ -4,8 +4,8 @@ export default function AccountLayout({ children }: { children: React.ReactNode 
   return (
    <div className=" bg-[#f5f5fa] min-h-screen pt-4">
     <div className="flex gap-4 max-w-[1200px] mx-auto">
-      <AccountSidebar />
-      <main className="flex-1 bg-white rounded-md">
+      {/* <AccountSidebar /> */}
+      <main className="flex-1 bg-white rounded-md pb-[180px]">
         {children}
       </main>
     </div>
